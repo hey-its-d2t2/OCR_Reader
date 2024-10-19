d2df35fd47d30133f30d7b63b9baa4afdc23a891
Deepak Singh <deepsinghkumar01@gmail.com>
2024-10-19 10:38:20 +0530

    To centralize constant values, we use a Constants class.

---
+++ src/main/java/com/OCR/Reader/constants/AppConstants.java
@@ -0,0 +1,7 @@
+package com.OCR.Reader.constants;
+
+public class AppConstants {
+    public static final String SUCCESS = "SUCCESS";
+    public static final String ERROR = "ERROR";
+    public static final String LANGUAGE = "hin+eng"; // For OCR in Hindi and English
+}
