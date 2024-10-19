5a7ed06ce0ed43fae00b79b93770b6ba34c64bdd
Deepak Singh <deepsinghkumar01@gmail.com>
2024-10-19 10:16:31 +0530

    Initial commit, project created form start.spring.io;

---
+++ src/main/java/com/OCR/Reader/OcrReaderApplication.java
@@ -0,0 +1,13 @@
+package com.OCR.Reader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcrReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OcrReaderApplication.class, args);
+	}
+
+}
