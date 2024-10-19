c2a6f0affdea5762ce2f6a0108f34862609c04e5
Deepak Singh <deepsinghkumar01@gmail.com>
2024-10-19 11:03:14 +0530

    Method implemented,

--- src/main/java/com/OCR/Reader/service/impl/OCRServiceImpl.java
+++ src/main/java/com/OCR/Reader/service/impl/OCRServiceImpl.java
@@ -1,17 +1,53 @@
 package com.OCR.Reader.service.impl;
 
+import com.OCR.Reader.config.OCRProcessor;
+import com.OCR.Reader.constants.AppConstants;
 import com.OCR.Reader.pojo.OCRResult;
 import com.OCR.Reader.service.OCRService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+
+@Service
+@Slf4j
 public class OCRServiceImpl implements OCRService {
     @Override
     public OCRResult processImage(MultipartFile file) {
-        return null;
+
+        OCRResult result = new OCRResult();
+        try {
+            //Save file to a temp location
+
+            File tempFile = File.createTempFile("ocr",".tmp");
+            file.transferTo(tempFile);
+
+            //Perform OCR
+            OCRProcessor ocrProcessor = new OCRProcessor();
+            String extractedText = ocrProcessor.extractTextFromImage(tempFile);
+
+            // Clean up the temp file
+            tempFile.delete();
+
+            result.setExtractedText(extractedText);
+            result.setStatus(AppConstants.SUCCESS);
+
+        } catch (IOException e) {
+           log.info("Error in processing image::"+e);
+            result.setStatus(AppConstants.ERROR);
+        }
+
+        return result;
     }
 
     @Override
     public String searchInText(String extractedText, String keyword) {
-        return "";
+        if (extractedText != null && extractedText.contains(keyword)) {
+            return "Keyword found!";
+        }
+        return "Keyword not found.";
     }
+
 }
