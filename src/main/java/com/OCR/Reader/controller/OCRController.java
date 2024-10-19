1a81b4d1a777841e190c7eb1deef58e0a7cb81e5
Deepak Singh <deepsinghkumar01@gmail.com>
2024-10-19 12:15:33 +0530

    Remove unused imports.

--- src/main/java/com/OCR/Reader/controller/OCRController.java
+++ src/main/java/com/OCR/Reader/controller/OCRController.java
@@ -3,7 +3,6 @@ package com.OCR.Reader.controller;
 import com.OCR.Reader.pojo.OCRResult;
 import com.OCR.Reader.service.OCRService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -17,14 +16,12 @@ public class OCRController {
     // Endpoint for image upload and OCR processing
     @PostMapping("/upload")
     public OCRResult handleFileUpload(@RequestParam("imageFile") MultipartFile imageFile) {
-        // Process the uploaded image and extract text
         return ocrService.processImage(imageFile);
     }
 
     // Endpoint for searching a keyword in extracted text
     @PostMapping("/search")
     public String searchInText(@RequestParam("keyword") String keyword, @RequestParam("extractedText") String extractedText) {
-        // Search keyword in the extracted text and return the result
         return ocrService.searchInText(extractedText, keyword);
     }
 }
