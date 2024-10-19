ac3b9ba58285a6ee6b3c6650414ab21568d60088
Deepak Singh <deepsinghkumar01@gmail.com>
2024-10-19 11:04:00 +0530

    Path variable added for path_to_tessdata_folder,

--- src/main/java/com/OCR/Reader/constants/AppConstants.java
+++ src/main/java/com/OCR/Reader/constants/AppConstants.java
@@ -4,4 +4,5 @@ public class AppConstants {
     public static final String SUCCESS = "SUCCESS";
     public static final String ERROR = "ERROR";
     public static final String LANGUAGE = "hin+eng"; // For OCR in Hindi and English
+    public static final String PATH ="D:\\PSA-Java\\MyProj\\ORC-Reader\\Tess4J-3.4.8-src\\Tess4J\\tessdata"; //path_to_tessdata_folder
 }
