790ac509f1056c046a6908790c82b505bc82315f
Deepak Singh <deepsinghkumar01@gmail.com>
2024-10-19 13:59:52 +0530

    Changed for eng ver,

--- src/main/java/com/OCR/Reader/constants/AppConstants.java
+++ src/main/java/com/OCR/Reader/constants/AppConstants.java
@@ -3,6 +3,6 @@ package com.OCR.Reader.constants;
 public class AppConstants {
     public static final String SUCCESS = "SUCCESS";
     public static final String ERROR = "ERROR";
-    public static final String LANGUAGE = "hin+eng"; // For OCR in Hindi and English
+    public static final String LANGUAGE = "eng+hin"; // For OCR in Hindi and English //replace hin+eng for hindi result
     public static final String PATH ="D:\\PSA-Java\\MyProj\\ORC-Reader\\Tess4J-3.4.8-src\\Tess4J\\tessdata"; //path_to_tessdata_folder
 }
